8ce2a30e6a8354aaf21cef72be3e48b0d8eca562
Enrique Cruz <eac2192@columbia.edu>
2013-04-16 16:39:05 -0400

    wrote greyscale and threshhold methods

--- process.py
+++ process.py
@@ -5,7 +5,9 @@ import numpy as np
 if len(sys.argv) != 2:
   print "Usage : python process.py <image_file>"
 else:
-  img = cv2.imread(sys.argv[1], cv2.CV_LOAD_IMAGE_COLOR)
+  color_img = cv2.imread(sys.argv[1], cv2.CV_LOAD_IMAGE_COLOR)
+  img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+
   if (img == None):
     print "Could not open or find the image"
   else:
@@ -19,9 +21,22 @@ else:
 class ColorLapse
   def __init__(self, filename):
     self.filename = filename
+
   def greyscale(self):
+    self.image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2GRAY)
 
   def normalize(self):
+    return None
 
   def threshold(self):
-    return 1
+    thresh_val = 20
+    cols, rows = self.image.shape()
+    for row in range(cols):
+      for cols in range(rows):
+        if self.image[col][row] < thresh_val:
+          self.image[col][row] = 0
+        else:
+          self.image[col][row] = 255
+
+if __name__ == "__main__":
+  
