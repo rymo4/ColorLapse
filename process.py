f6411da8f1e3b86df9b937b16c5edc3c521c783b
Ryder Moody <rydthemoodster@gmail.com>
2013-04-16 16:40:21 -0400

    Add normalizatio

--- process.py
+++ process.py
@@ -2,26 +2,30 @@ import cv2
 import sys
 import numpy as np
 
-if len(sys.argv) != 2:
-  print "Usage : python process.py <image_file>"
-else:
-  img = cv2.imread(sys.argv[1], cv2.CV_LOAD_IMAGE_COLOR)
-  if (img == None):
-    print "Could not open or find the image"
-  else:
-    window = 'window'
-    cv2.namedWindow(window)
-    cv2.imshow(window, img)
-    print "size of image: ", img.shape
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
-class ColorLapse
+class ColorLapse:
   def __init__(self, filename):
     self.filename = filename
+    self.color_img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_COLOR)
+    self.img = None
+    self.window = 'window'
+
   def greyscale(self):
+    return 1
 
-  def normalize(self):
+  # spreads the intensities eveny throught the spectrum
+  def normalize(self, img):
+    return img * 255.0/img.max()
 
+  # turns the  image into pure black and white
   def threshold(self):
     return 1
+
+if __name__ == "__main__":
+  cl = ColorLapse(sys.argv[1])
+  cv2.namedWindow(cl.window)
+
+  cv2.imshow(cl.window, cl.color_img)
+
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
