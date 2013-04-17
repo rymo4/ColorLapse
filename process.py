e39c8e588f6eba4272a086c4129a4bcac600781e
Ryder Moody <rydthemoodster@gmail.com>
2013-04-17 14:07:46 -0400

    Allow ink coloring

--- process.py
+++ process.py
@@ -3,15 +3,16 @@ import sys
 import numpy as np
 
 class ColorLapse:
-  def __init__(self, filename):
+  def __init__(self, filename, pen_color):
     self.filename = filename
     self.color_img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_COLOR)
     self.img = None
     self.window = 'window'
-    self.threshold()
+    self.threshold_ink()
+    self.color_ink(pen_color)
 
   # turns the  image into pure black and white
-  def threshold(self):
+  def threshold_ink(self):
     self.img = np.zeros((self.color_img.shape[0],self.color_img.shape[1]), dtype=self.color_img.dtype)
     thresh_val = 50
     rows, cols, temp = self.color_img.shape
@@ -23,6 +24,18 @@ class ColorLapse:
           self.expand_pixel(row, col)
         else:
           self.img[row][col] = 255
+
+  def color_ink(self, color):
+    self.img = abs(self.img - 255)
+    for i in range(3):
+      self.color_img[:,:,i] = self.img * color[i]
+    rows, cols, temp = self.color_img.shape
+    for row in range(rows):
+      for col in range(cols):
+        rgb = self.color_img[row][col]
+        if rgb[0] == 0 and rgb[1] == 0 and rgb[2] == 0:
+          self.color_img[row][col]= np.array([255, 255, 255])
+
 
   def expand_pixel(self, row, col):
     width, height, t = self.color_img.shape
@@ -38,8 +51,8 @@ class ColorLapse:
     self.img[row][col+1] = 0
 
 if __name__ == "__main__":
-  cl = ColorLapse(sys.argv[1])
+  cl = ColorLapse(sys.argv[1], map(int, sys.argv[2:5]))
   cv2.namedWindow(cl.window)
-  cv2.imshow(cl.window, cl.img)
+  cv2.imshow(cl.window, cl.color_img)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
