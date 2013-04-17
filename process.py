dfee29734a08143d85ed065200db942eab78ae35
Ryder Moody <rydthemoodster@gmail.com>
2013-04-16 21:34:33 -0400

    Expand region around pixels and remove old code

--- process.py
+++ process.py
@@ -8,17 +8,7 @@ class ColorLapse:
     self.color_img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_COLOR)
     self.img = None
     self.window = 'window'
-    #self.greyscale()
-    #self.normalize()
     self.threshold()
-
-  # spreads the intensities eveny throught the spectrum
-  def normalize(self):
-    self.img *= 255.0/self.img.max()
-
-  def greyscale(self):
-    self.img = np.zeros((self.color_img.shape[0],self.color_img.shape[1]), dtype=self.color_img.dtype)
-    self.img [:,:] = self.color_img[:,:,0]
 
   # turns the  image into pure black and white
   def threshold(self):
@@ -30,12 +20,22 @@ class ColorLapse:
         #print self.color_img[row][col]
         if self.color_img[row][col][2] > 140 and not (self.color_img[row][col][1] > 50 or self.color_img[row][col][0] > 50):
           self.img[row][col] = 0
+          self.expand_pixel(row, col)
         else:
           self.img[row][col] = 255
 
-  # goes through the pixel and expands those that are colored 
-  def expand_pixels(self):
-    return -1
+  def expand_pixel(self, row, col):
+    width, height, t = self.color_img.shape
+    if row <= 1 or row >= (width - 1) or col <= 1 or col >= (height -1):
+      return
+    self.img[row-1][col] = 0
+    self.img[row+1][col] = 0
+    self.img[row-1][col-1] = 0
+    self.img[row-1][col+1] = 0
+    self.img[row+1][col-1] = 0
+    self.img[row+1][col+1] = 0
+    self.img[row][col-1] = 0
+    self.img[row][col+1] = 0
 
 if __name__ == "__main__":
   cl = ColorLapse(sys.argv[1])
